0c2d172ddbd192d055a1442b627ff8732f5e4dda
Louis Cundari III <louiec1212@yahoo.com>
2022-07-02 15:13:30 -0400

    Inital Commit

---
+++ csgo_skins_data.py
@@ -0,0 +1,106 @@
+import requests
+import json
+import pickle
+
+import pandas as pd
+# from pandas import json_normalize
+import numpy as np
+import random
+
+from datetime import datetime
+import time
+
+# testdf = pd.DataFrame(columns={'test'})
+# testdf.to_csv('steamdf.csv', index=False)
+# quit()
+
+cookie = {'steamLoginSecure': '76561198071584305%7C%7C3A8F0AB8B591CD5A91DA8F78E21F932DAB553855'}
+
+game = 730
+weaponList = ['Pistol', 'SMG', 'Rifle', 'SniperRifle', 'Shotgun', 'Machinegun', 'Knife']
+weaponList = ['Pistol']
+# gameList = ['252490','578080']
+
+for weapon in weaponList:
+    print(weapon)
+    # itialize
+    allItemNames = []
+    
+    # find total number items
+    # allItemsGet = requests.get('https://steamcommunity.com/market/search/render/?search_descriptions=0&sort_column=default&sort_dir=desc&appid=' + gameID + '&norender=1&count=100', cookies=cookie); # get page
+    marketLink = "https://steamcommunity.com/market/search/render/"
+    requestParams = {
+		"query": "",
+		"start": 1,
+		"count": 1000,
+		"search_descriptions": 0,
+		"sort_column": "name",
+		"sort_dir": "asc",
+		"appid": game,
+		"category_730_ItemSet[]": "any",
+		"category_730_ProPlayer[]": "any",
+		"category_730_StickerCapsule[]": "any",
+		"category_730_TournamentTeam[]": "any",
+		"category_730_Weapon[]": "any",
+        "category_730_Type[]": f'tag_CSGO_Type_{weapon}',
+		"norender": 1
+	}
+    
+    weaponGet = requests.get(marketLink, requestParams, cookies=cookie)
+
+    allItems = weaponGet.content; # get page content
+    # print(weaponGet.text)
+    # quit()
+    
+    allItems = json.loads(allItems); # convert to JSON
+    totalItems = allItems['total_count']; # get total count
+    print(totalItems)
+
+    # you can only get 100 items at a time (despite putting in count= >100)
+    # so we have to loop through in batches of 100 to get every single item name by specifying the start position
+    # for currPos in range(0,totalItems + 100,100): # loop through all items
+    for currPos in range(0, 1): # loop through all items
+        # time.sleep(random.uniform(0.5, 2.5)) # you cant make requests too quickly or steam gets mad
+        # time.sleep(.) # you cant make requests too quickly or steam gets mad
+        requestParams['start'] = currPos
+        
+        # get item name of each
+        # weaponGet = requests.get('https://steamcommunity.com/market/search/render/?start=' + str(currPos) + '&count=100&search_descriptions=0&sort_column=default&sort_dir=desc&appid=' + weapon + '&norender=1&count=5000', cookies=cookie)
+        weaponGet = requests.get(marketLink, requestParams, cookies=cookie)
+        print('Items ' + str(currPos)+' out of ' + str(totalItems) + ' ' + str(weaponGet.status_code)) # reassure us the code is running and we are getting good returns (code 200)
+        
+        allItems = weaponGet.content
+        allItems = json.loads(allItems)
+        allItems = allItems['results']
+        for currItem in allItems: 
+            allItemNames.append(currItem['hash_name']) # save the names
+
+    allItemNames = list(set(allItemNames))
+    
+    # Save all the name so we don't have to do this step anymore
+    # use pickle to save all the names so i dont have to keep running above code
+    with open(weapon + 'ItemNames.json', 'w') as file:
+        json.dump(allItems, file, ensure_ascii=True)
+
+    # with open(weapon + 'ItemNames.txt', "wb") as file: # change the text file name to whatever you want
+    #     pickle.dump(allItemNames, file)
+    
+    print('test')
+    print(file)
+    # print(allItemNames)
+    # quit()
+    
+for weapon in weaponList:
+    # open file with all names
+    with open(weapon+'ItemNames.json', "rb") as file:   # Unpickling
+       weaponSkinsDict = json.loads(file)
+    
+    
+    print(weaponSkinsDict)
+    
+    # intialize our Panda's dataframe with the data we want from each item
+    weapons_df = pd.DataFrame.from_dict(weaponSkinsDict, orient='index')
+    weapons_df.reset_index(level=0, inplace=True)
+    print(weapons_df)
+    # allItemsPD = pd.DataFrame(data=None,index=None,columns = ['itemName','initial','timeOnMarket','priceIncrease','priceAvg','priceSD','maxPrice','maxIdx','minPrice','minIdx','swing','volAvg','volSD','slope','rr']);
+    # currRun = 1; # to keep track of the program running
